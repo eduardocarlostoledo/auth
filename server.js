d5a94963b51b90dd25e2d07f735eac2c059cb210
eduardocarlostoledo <113210953+eduardocarlostoledo@users.noreply.github.com>
2023-02-10 12:24:44 -0300

    hw auth0 routes server.js

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
+const cookieparser = require('cookie-parser') //`cookie-parser` que nos va a permitir justamente parsear los datos de la cookie enviada dentro del header de los request HTTP y agregarlo dentro del objeto `req`, particularmente en un atributo llamado `req.cookies`.
 const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
 
 const users = [
-  {id: 1, name: 'Franco', email: 'Franco@mail.com', password: '1234'},
-  {id: 2, name: 'Toni', email: 'Toni@mail.com', password: '1234'}
+  {id: 1, name: 'admin', email: 'admin@admin.com', password: '1111'},
+  {id: 2, name: 'web', email: 'web@web.com', password: '1111'}
 ]
 
 app.use(morgan('dev'));
+app.use(cookieparser());
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Bienvenidos a Henry!</h1>
+    <h1>Home</h1>
   `)
 });
 
@@ -22,4 +24,31 @@ app.listen(3000, (err) => {
  } else {
    console.log('Listening on localhost:3000');
  }
+});
+
+//Para poder hacer un seguimiento de la cookie y los datos almacenados en ella vamos a definir un middleware que será ejecutado previo a cada request recibido por el servidor:
+
+app.use((req, res, next) => {
+  console.log(req.cookies);
+  next();
+});
+
+//PRIMERO INICIO Y REGISTRO
+
+app.get('/', (req, res) => {
+  res.send(`
+    
+    <h1> Bienvenido al Sitio </h1>
+    
+    ${req.cookies.userID 
+    ? 
+    `<a ref= '/home> Perfil Usuario </a>
+    <form method= 'post' action ='/logout'>
+    <button> Salir </button>
+    </form> 
+    ` : `
+    <a ref='/login'> Entrar </a>
+    <a ref='/register'> Registro </a>
+    `}    
+    `)
 });
